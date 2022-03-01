df2429594203f6b7a981aab3e6231a3105fb110c
Jiří Vrba <jiri@vrba.dev>
2022-03-01 08:30:11 +0100

    Add revocation to member verification dto

--- Dto/MemberVerificationDto.cs
+++ Dto/MemberVerificationDto.cs
@@ -7,17 +7,21 @@ public class MemberVerificationDto
 {
     public MemberVerificationDto(MemberVerification verification)
     {
+        IsVerified = verification.AzureId != null && !verification.IsRevoked;
         VerificationId = verification.Id.ToString();
         DiscordMemberId = verification.DiscordId.ToString();
         AzureId = verification.AzureId;
     }
-    
+
+    [JsonPropertyName("is_verified")]
+    public bool IsVerified;
+
     [JsonPropertyName("verification_id")]
-    public string VerificationId { get; }
-    
+    public string VerificationId;
+
     [JsonPropertyName("discord_member_id")]
-    public string DiscordMemberId { get; }
-    
+    public string DiscordMemberId;
+
     [JsonPropertyName("azure_id")]
-    public string? AzureId { get; }
+    public string? AzureId;
 }
